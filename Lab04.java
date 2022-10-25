8b33b42abf4001b4cf3e83a1f9c82047783383d7
aleksl11 <110901701+aleksl11@users.noreply.github.com>
2022-10-25 11:16:10 +0200

    Update and rename Main.java to Lab04.java

--- Lab04.java
+++ Lab04.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Lab04 {
     public static void main(String[] args) {
         zadanie7();
     }
