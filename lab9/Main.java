be36b1710a22ac783a14415590ed22b1f8bf13c2
aleksl11 <110901701+aleksl11@users.noreply.github.com>
2022-11-29 11:33:58 +0100

    Update Main.java

--- lab9/Main.java
+++ lab9/Main.java
@@ -1,19 +1,17 @@
 import java.util.*;
 
 class Uczestnicy {
-    int ID,wiek;
+    int wiek;
     String imie;
 
-    public Uczestnicy(int ID, int wiek, String imie) {
-        this.ID = ID;
+    public Uczestnicy(int wiek, String imie) {
+        
         this.wiek = wiek;
         this.imie = imie;
     }
     @Override
     public String toString(){
-        return("ID uczestnika: "+ID+
-                        "\nWiek: "+wiek+
-                        "\nImie: "+imie);
+        return("Wiek: "+wiek+ "\nImie: "+imie);
     }
 
     @Override
@@ -21,12 +19,12 @@ class Uczestnicy {
         if (this == o) return true;
         if (!(o instanceof Uczestnicy)) return false;
         Uczestnicy that = (Uczestnicy) o;
-        return ID == that.ID && wiek == that.wiek && imie.equals(that.imie);
+        return  wiek == that.wiek && imie.equals(that.imie);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ID, wiek, imie);
+        return Objects.hash(wiek, imie);
     }
 }
 
@@ -36,18 +34,18 @@ public class Main {
     }
     public static void zadanie1(){
         List<Uczestnicy> u = new ArrayList<>();
-        Uczestnicy u1= new Uczestnicy(1,19,"Adam");
-        Uczestnicy u2= new Uczestnicy(2,17,"Ala");
-        Uczestnicy u3= new Uczestnicy(3,16,"Jan");
-        Uczestnicy u4= new Uczestnicy(4,20,"Ewa");
+        Uczestnicy u1= new Uczestnicy(19,"Adam");
+        Uczestnicy u2= new Uczestnicy(17,"Ala");
+        Uczestnicy u3= new Uczestnicy(16,"Jan");
+        Uczestnicy u4= new Uczestnicy(20,"Ewa");
 
         u.add(u1);
         u.add(u2);
         u.add(u3);
         u.add(u4);
         u.add(u2);
-        u.add(new Uczestnicy(5,18,"Kamil"));
-        u.add(new Uczestnicy(6,19,"Piotr"));
+        u.add(new Uczestnicy(18,"Kamil"));
+        u.add(new Uczestnicy(19,"Piotr"));
 
         System.out.println(u1.equals(u1));
         System.out.println(u1.equals(u2));
